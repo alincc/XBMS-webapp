8133de43f109ec5d7d68c42d6fe5a11a10fce4e8
psombekke@intermadix.com <psombekke@intermadix.com>
2019-12-04 17:18:27 +0100

    update

--- src/app/marketing/videocreator/videocreator.component.ts
+++ src/app/marketing/videocreator/videocreator.component.ts
@@ -1958,6 +1958,11 @@ export class VideocreatorComponent implements OnInit {
       // example originalsize = {x: 0, y: 0, width: 1496, height: 1496, zoom: 0.06684491978609626}
       let idto = idx;
       let p = idto.getElementsByTagName("path");
+
+      let newsvg = this.get_metrics(idto);
+      console.log(newsvg);
+
+
       for (let index = 0; index < p.length; index++) {
 
         if (p[index].id === '') {
@@ -1972,7 +1977,7 @@ export class VideocreatorComponent implements OnInit {
         let oripath = MotionPathPlugin.getRawPath(p[index]); //p[index].attributes['d'].value;
         oripath =  MotionPathPlugin.rawPathToString(oripath);
         console.log(oripath, svgtrans);
-
+        p[index].setAttribute("transform", '');
         // m 5333.91,1077.5 h 106.23 v 408.648 h -106.23 z
         //oripath = oripath.replace('z', '');
 
@@ -1980,24 +1985,93 @@ export class VideocreatorComponent implements OnInit {
         // matrix(0.13333332737286877,0,0,-0.13333332737286877,0,1040)
         //
 
-        if (svgtrans !== null && svgtrans !== 'matrix(1 0 0 1 0 0)') {
-          svgtrans = svgtrans.replace('matrix(', '');
-          svgtrans = svgtrans.replace(')', '');
-          let svgtransarray = svgtrans.split(' ').map(Number);
-          // svgtransarray.forEach((value, index) => {
-          //   svgtransarray[index] = Math.round(value);
-          // })
-          console.log (oripath, svgtransarray[0], svgtransarray[1], svgtransarray[2], svgtransarray[3], svgtransarray[4], svgtransarray[5]);
-          rawpath = MotionPathPlugin.transformRawPath(oripath, svgtransarray[0], svgtransarray[1], svgtransarray[2], svgtransarray[3], svgtransarray[4], svgtransarray[5]);
-          p[index].setAttribute("d", rawpath);
-          p[index].setAttribute("transform", '');
-        }
+        // if (svgtrans !== null && svgtrans !== 'matrix(1 0 0 1 0 0)') {
+        //   svgtrans = svgtrans.replace('matrix(', '');
+        //   svgtrans = svgtrans.replace(')', '');
+        //   let svgtransarray = svgtrans.split(' ').map(Number);
+        //   // svgtransarray.forEach((value, index) => {
+        //   //   svgtransarray[index] = Math.round(value);
+        //   // })
+        //   console.log (oripath, svgtransarray[0], svgtransarray[1], svgtransarray[2], svgtransarray[3], svgtransarray[4], svgtransarray[5]);
+        //   rawpath = MotionPathPlugin.transformRawPath(oripath, svgtransarray[0], svgtransarray[1], svgtransarray[2], svgtransarray[3], svgtransarray[4], svgtransarray[5]);
+        //   p[index].setAttribute("d", rawpath);
+        //   p[index].setAttribute("transform", '');
+        // }
+
+        // SVGElement.prototype.getTransformToElement = 
+        // SVGElement.prototype.getTransformToElement || function(elem)
+        // { 
+        //   return elem.getScreenCTM().inverse().multiply(this.getScreenCTM()); 
+        // };
+
       }
 
         resolve();
       }
     });
   }
+
+
+// https://stackoverflow.com/questions/13329125/removing-transforms-in-svg-files
+get_metrics(el) {
+    function pointToLineDist(A, B, P) {
+        var nL = Math.sqrt((B.x - A.x) * (B.x - A.x) + (B.y - A.y) * (B.y - A.y));
+        return Math.abs((P.x - A.x) * (B.y - A.y) - (P.y - A.y) * (B.x - A.x)) / nL;
+    }
+
+    function dist(point1, point2) {
+        var xs = 0,
+            ys = 0;
+        xs = point2.x - point1.x;
+        xs = xs * xs;
+        ys = point2.y - point1.y;
+        ys = ys * ys;
+        return Math.sqrt(xs + ys);
+    }
+    var b = el.getBBox(),
+        objDOM = el,
+        svgDOM = objDOM.ownerSVGElement;
+    // Get the local to global matrix
+    var matrix = svgDOM.getTransformToElement(objDOM).inverse(),
+        oldp = [[b.x, b.y], [b.x + b.width, b.y], [b.x + b.width, b.y + b.height], [b.x, b.y + b.height]],
+        pt, newp = [],
+        obj = {},
+        i, pos = Number.POSITIVE_INFINITY,
+        neg = Number.NEGATIVE_INFINITY,
+        minX = pos,
+        minY = pos,
+        maxX = neg,
+        maxY = neg;
+
+    for (i = 0; i < 4; i++) {
+        pt = svgDOM.createSVGPoint();
+        pt.x = oldp[i][0];
+        pt.y = oldp[i][1];
+        newp[i] = pt.matrixTransform(matrix);
+        if (newp[i].x < minX) minX = newp[i].x;
+        if (newp[i].y < minY) minY = newp[i].y;
+        if (newp[i].x > maxX) maxX = newp[i].x;
+        if (newp[i].y > maxY) maxY = newp[i].y;
+    }
+    // The next refers to the transformed object itself, not bbox
+    // newp[0] - newp[3] are the transformed object's corner
+    // points in clockwise order starting from top left corner
+    obj['newp'] = newp; // array of corner points
+    obj['width'] = pointToLineDist(newp[1], newp[2], newp[0]) || 0;
+    obj['height'] = pointToLineDist(newp[2], newp[3], newp[0]) || 0;
+    obj['toplen'] = dist(newp[0], newp[1]);
+    obj['rightlen'] = dist(newp[1], newp[2]);
+    obj['bottomlen'] = dist(newp[2], newp[3]);
+    obj['leftlen'] = dist(newp[3], newp[0]);
+    // The next refers to the transformed object's bounding box
+    obj['BBx'] = minX;
+    obj['BBy'] = minY;
+    obj['BBx2'] = maxX;
+    obj['BBy2'] = maxY;
+    obj['BBwidth'] = maxX - minX;
+    obj['BBheight'] = maxY - minY;
+    return obj;
+}
 
 
   async renumberSvgIds(svgstring, idx, pathidar) {
